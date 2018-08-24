56ba224a179da182b385cd5e1a48271719a598ba
Dilek Hikmet Konduk <dilek.konduk@fortum.com>
2018-08-24 20:02:17 +0200

    post

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var qs = require("querystring");
+var http = require("https");
 
-var params = function (req) {
-    let q = req.url.split('?'), result = {};
-    if (q.length >= 2) {
-        q[1].split('&').forEach((item) => {
-            try {
-                result[item.split('=')[0]] = item.split('=')[1];
-            } catch (e) {
-                result[item.split('=')[0]] = '';
-            }
-        })
-    }
-    return result;
-}
+var options = {
+  "method": "POST",
+  "hostname": [
+    "testapi",
+    "fortum",
+    "com"
+  ],
+  "path": [
+    "TSU",
+    "v1",
+    "Web2Leads"
+  ],
+  "headers": {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "RequestingSystem": "WEB",
+    "TargetSystem": "TSU",
+    "Cache-Control": "no-cache",
+    "Postman-Token": "a1437f37-9a88-4675-bcc7-0949d5afdf5b"
+  }
+};
 
-http.createServer(function (req, res) {
-    req.params = params(req);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    console.log('DHK ECO TEST ' + 'URL Input:\n ' + req.url);
-    res.end('DHK ECO TEST '+'URL Input:\n '+req.url);
-}).listen(port);
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.write(qs.stringify({ CipherText: 'tNR4HLTUYLJSoWZU%2BdaeaauEaYeRHjH3aHhybr8ZfdfhAy5QXKPe47ANQGNHFAVIc6%2FNij9WGDYq3oe5nHIAAP5SSqgopLlr94%2FekK9FRsg%3D&Type=',
+  FirstName: 'Dilek',
+  LastName: 'Konduk',
+  EmailAddress: 'dilek.konduk@test007.com',
+  MobilePhone: '07392612121212',
+  AdditionalInfo1: '1100110055' }));
+req.end();
