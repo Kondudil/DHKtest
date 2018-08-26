5fcbaeeb4453d1a3849fedde67ab12a62f2602d8
Dilek Hikmet Konduk <dilek.konduk@fortum.com>
2018-08-27 00:34:21 +0200

    mater 2.0
    test master.

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-var unirest = require("unirest");
+var request = require("request");
 
-var req = unirest("POST", "https://testapi.fortum.com/TSU/v1/Web2Leads");
+var options = { method: 'POST',
+  url: 'https://testapi.fortum.com/TSU/v1/Web2Leads',
+  headers: 
+   { 'Postman-Token': '2b39082d-ba67-41d8-ad1f-55f37dbe3bf5',
+     'Cache-Control': 'no-cache',
+     TargetSystem: 'TSU',
+     RequestingSystem: 'WEB',
+     'Content-Type': 'application/x-www-form-urlencoded' },
+  form: 
+   { CipherText: 'tNR4HLTUYLJSoWZU%2BdaeaauEaYeRHjH3aHhybr8ZfdfhAy5QXKPe47ANQGNHFAVIc6%2FNij9WGDYq3oe5nHIAAP5SSqgopLlr94%2FekK9FRsg%3D&Type=',
+     FirstName: 'Dilek',
+     LastName: 'Konduk',
+     EmailAddress: 'dilek.konduk@test007.com',
+     MobilePhone: '07392612121212',
+     AdditionalInfo1: '1100110055' } };
 
-req.headers({
-  "Postman-Token": "04b90ab1-42a6-4897-8c96-3551dd30d162",
-  "Cache-Control": "no-cache",
-  "TargetSystem": "TSU",
-  "RequestingSystem": "WEB",
-  "Content-Type": "application/x-www-form-urlencoded"
-});
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
 
-req.form({
-  "CipherText": "tNR4HLTUYLJSoWZU%2BdaeaauEaYeRHjH3aHhybr8ZfdfhAy5QXKPe47ANQGNHFAVIc6%2FNij9WGDYq3oe5nHIAAP5SSqgopLlr94%2FekK9FRsg%3D&Type=",
-  "FirstName": "Dilek",
-  "LastName": "Konduk",
-  "EmailAddress": "dilek.konduk@test0007.com",
-  "MobilePhone": "07392612121212",
-  "AdditionalInfo1": "1100110055"
-});
-
-req.end(function (res) {
-  if (res.error) throw new Error(res.error);
-
-  console.log(res.body);
+  console.log(body);
 });
